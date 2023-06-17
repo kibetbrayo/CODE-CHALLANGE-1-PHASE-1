a778c9c2fb7945f032005b3392a8de1ac2562eb3
iam <brian.kibet2@student.moringaschool.com>
2023-06-17 12:27:52 +0000

    2nd commit

---
+++ speeddetector/index.js
@@ -0,0 +1,18 @@
+function checkspeed() {
+    const speed = parseFloat(prompt("Enter the car speed:"));
+  
+    if (speed < 70) {
+      return "OK";
+    } else {
+      const demeritpoints = Math.floor((speed - 70) / 5);
+  
+      if (demeritpoints > 12) {
+        return "License suspended";
+      } else {
+        return "Points: " + demeritpoints;
+      }
+    }
+  }
+  
+  console.log(checkspeed());
+  
